d3c06ce99c3a34bc7dc9f3bd8f8f6d508526d194
Ankur Gupta <rukna1000@gmail.com>
2017-09-10 01:56:46 -0400

    Just added some basic comments for the fireworks program, going to work on and hopefully finish it tomorrow

---
+++ assignment2/fireworks.rkt
@@ -0,0 +1,12 @@
+(require 2hdtp/image)
+(require 2hdtp/universe)
+
+;; ===================
+;; Data Definitions
+
+(define-struct ws (lof color size fuse))
+;; ===================
+;; Constants
+
+;; ===================
+;; Functions
