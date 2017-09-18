103ce3008371d43ca9d5313c9be3c0ab13bddfde
Ankur Gupta <rukna1000@gmail.com>
2017-09-18 16:37:00 -0400

    Theorectially, done with the render function.. time to test it out !

---
+++ assignment3/part3/display widgets.rkt
@@ -0,0 +1,59 @@
+(require 2htdp/image)
+
+(define-struct widget(name quantity time price parts))
+;; a widget is a (make-widget String Natural Natural Number ListOfWidget)
+
+(define Wire (make-widget "Wire" 3 5 5 empty))
+(define Cord (make-widget "Cord" 7 5 5 (list Wire)))
+(define Numbers (make-widget "Numbers" 9 5 5 empty))
+(define Buttons (make-widget "Buttons" 8 5 5 (list Numbers)))
+(define Cell (make-widget "Cell" 6 25 7 (list Buttons)))
+(define Receiver (make-widget "Receiver" 10 5 7 empty))
+(define Telephone (make-widget "Telephone" 5 20 15 (list Receiver Buttons Cord)))
+
+(define Glass (make-widget "Glass" 6 9 4 empty))
+(define Beads (make-widget "Beads" 25 12 7 (list Glass)))
+(define Bracelet (make-widget "Bracelet" 5 3 5 (list Beads)))
+(define Chain (make-widget "Chain" 7 2 1 empty))
+(define Pendant (make-widget "Pendant" 4 3 1 empty))
+(define Necklace (make-widget "Necklace" 10 7 3 (list Chain Pendant)))
+(define Rings (make-widget "Rings" 15 8 11 empty))
+(define Jewelry (make-widget "Jewelry" 4 17 30 (list Rings Necklace Bracelet)))
+
+(define TEXT-SIZE 24)    
+(define TEXT-COLOR "black")
+(define TAB 5)
+
+;; Natural -> String
+;; creates a blank string of length equal to n
+(check-expect (blanks 0) "")
+(check-expect (blanks 3) "   ")
+
+(define (blanks n)
+  (list->string (build-list n (lambda (x) #\ ))))
+
+;; widget fn -> Image
+;; Given a widget an a function, this function will create a hierachy in which the sub widgets are indented in and the image colors are those according to function inputted
+
+(define (render widget fn)
+  (local [(define (render--w wid n)
+			(above/align 
+			  "left"
+			  (text (string-append 
+					  (blanks (* TAB n)) 
+					  (widget-name wid) 
+					  " : "
+					  (number->string (widget-quantity wid))
+					  " @ $"
+					  (number->string (widget-price wid)))
+					TEXT-SIZE
+					(fn wid))
+			  (render--low (widget-parts wid) (+ n 1))))
+		  (define (render--low low n)
+			(cond [(empty? low) (square 0 "solid" "white")]
+				  [else
+					(above/align
+					  "left"
+					  (render--w (first low) n)
+					  (render--low (rest low) n))]))]
+	(render--w widget)))
