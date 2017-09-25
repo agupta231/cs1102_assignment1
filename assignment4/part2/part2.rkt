fcfcd83268e16c5796e4f4606c274027445eda3d
Ankur Gupta <rukna1000@gmail.com>
2017-09-25 14:26:24 -0400

    Added starting code for part 2

---
+++ assignment4/part2/part2.rkt
@@ -0,0 +1,31 @@
+(define TEXT-SIZE 24)    
+(define TEXT-COLOR "black")
+(define TAB 5) 
+
+;; (value BST -> Boolean) (value BST -> Boolean) value bst -> widget | false
+;; Given a value, and BST, will return a widget whose value is the same as the one inputted.
+;;    This is done by the equality function and the less than function to deteremine how the 
+;;    tree should be traversed
+
+(define (find eq lt value bst)
+  (cond [(false? bst) false]
+        [else
+         (cond [(eq value BST) (bst-widget bst)]
+               [(lt value BST)
+                (find eq lt value (bst-left bst))]
+               [else
+                (find eq lt value (bst-right bst))])]))
+
+;; (widget BST -> Boolean) Widget BST -> BST
+;; Given a widget and BST, will return a BST with the widget inserted in the proper location
+;;     as dicated by the inutted comparison function
+
+(define insert (compare w bst)
+ (cond [(false?  BST) (make-bst w false false)]
+        [else
+         (if (compare w bst)
+             (make-bst (bst-widget BST) (bst-left BST) (insert w (bst-right BST)))
+             (make-bst (bst-widget BST) (insert w (bst-left BST)) (bst-right BST)))]))
+
+;; (X X -> Boolean) (Widget -> X) BST -> BST
+;
