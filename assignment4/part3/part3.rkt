fc95b836dae19d53f2997d50f7f695e31e086bbd
Ankur Gupta <rukna1000@gmail.com>
2017-09-26 01:54:46 -0400

    Most of part 3 should be done... Just need to clean up the main function tomorrow.

--- assignment4/part3/part3.rkt
+++ assignment4/part3/part3.rkt
@@ -4,34 +4,46 @@
 (require racket/list)
 (require 2htdp/image)
 
+;; =========== Data Definitions =============
+
+(define-struct widget (name quantity price))
+;; a widget is a (make-widget String Natural Number)
+; same as assignment #3, except no parts component
+
+(define-struct bst (widget left right))
+;; a BST is either
+;;          false, or
+;;          a (make-bst widget bst bst)
 
 
-
-
-;;;; OLD CODE ;;;;;;
+;; =========== Constants =============
 
 (define TEXT-SIZE 24)    
 (define TEXT-COLOR "black")  ;; not sure if i should have this???  maybe good for testing???
 (define TAB 5) ; someone senior told me this constant might help
 
+
+;; =========== Functions =============
+
+;; +++++++++ Taken from Part 1 ++++++++++
+
 ;; Natural -> String
 ;; creates a blank string of length equal to n
+
 (check-expect (blanks 0) "")
 (check-expect (blanks 3) "   ")
+
 (define (blanks n)
   (list->string (build-list n (lambda (x) #\ ))))
-
 
 ;;Natural Natural -> (listof Widget)
 ;;generates random widgets given a number of widgets to create and a range of possible numerical values for all parts of a widget
 
-;(define (random-widgets num range) (list (make-widget "0" 0 0))) ;stub
-
 (define (random-widgets num range)
   (build-list num (lambda (x) (make-widget (number->string (random range)) (random range) (random range)))))
 
-;;Widget Bst -> Bst
-;;adds widget to existing bst
+;; Widget Bst -> Bst
+;; adds widget to existing bst
 
 (define (insert w BST)
   (cond [(false?  BST) (make-bst w false false)]
@@ -81,60 +93,27 @@
     
     (render-element bst "T" 0)))
 
-;;;; OLD CODE ;;;;;;
+;; --------- Taken from Part 1 -----------
 
+;; +++++++++ Part 3 Functions Begin +++++++++++
 
-
-
-(define-struct widget (name quantity price))
-;; a widget is a (make-widget String Natural Number)
-; same as assignment #3, except no parts component
-
-(define-struct bst (widget left right))
-;; a BST is either
-;;          false, or
-;;          a (make-bst widget bst bst)
-
-
-
-;; sample code for Part 3
-
-; As you look over the sample code a thought occurs to you and
-; you confront The Head:  "Hey! You said we're not allowed to use
-; the list template, how come you're allowed to use it?"
-
-; "Because I don't need it.  However, in the restricted subset
-; of Racket you are using there isn't an easy way to solve this
-; problem with map, so I thought I'd give you an assist."
-
-; (there is a function called eval that evaluates code, but
-; it does not exist even in the advanced student language.)
+;; Function given from assignment
 
 (define (do-tests lst)
   (cond
     [(empty? lst) empty]
     [else
      (cons
-      ((first lst) 5) ; 5 is a dummy value
+      ((first lst) 5)
       (do-tests (rest lst)))]))
 
-; As The Head moves to leave you call out one last objection: "you
-; made a typo and have an extra set of parens on the second to
-; last line!"
+;; __________ Constants ______________
 
-; "That's not a typo.  That's the tricky part.  However, I'll also
-; give you an example of how to use the code."
+(define benchmarks (list 0.25 0.5 0.75 0)) ; Where in list values are taken from: 0 means not in list
 
-; an example of how your code should interact with do-tests
-; list1m is a list of 1 million elements
-; tree1m is the binary search tree constructed from list1m
+(define null-wid (make-widget "Gregor, the myth, the lengend" 420 1738)) ; Create placeholder for empty list search
 
-
-;; (listof Widget) BST -> (listof func)
-
-(define benchmarks (list 0.25 0.5 0.75 0))
-
-(define null-wid (make-widget "Gregor, the myth, the lengend" 420 1738))
+;; Build 10k 100k and 1M lists
 
 (define list10k (random-widgets 10000 10000000))
 (define bst10k (insert-all list10k false))
@@ -143,8 +122,9 @@
 (define list1M (random-widgets 1000000 10000000))
 (define bst1M (insert-all list1M false))
 
-(define list10 (random-widgets 10 100))
-(define bst10 (insert-all list10 false))
+;; (listof Widget) BST -> (listof func)
+;; Given a list of widgets and a bst, will produce a list of functions that will execute
+;;    timing tests for the different values scattered around the list and BST. 
 
 (define (generate-test-code lst bst)
   (build-list
@@ -171,16 +151,3 @@
                               (string=? search-val (widget-name y)))
                             lst)))))]
        (gen-test list_pos)))))
-
-
-
-(define (determine-wid val)
-  (cond [(zero? (floor
-                 (* (list-ref benchmarks (floor (/ val 2)))
-                    (length list10)))) 
-         null-wid] 
-        [else
-         (list-ref list10 
-                   (floor
-                    (* (list-ref benchmarks (floor (/ val 2)))
-                       (length list10))))]))
